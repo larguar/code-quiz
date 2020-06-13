a2840c0545c4a8aa3b45a20269630cd39eaf724d
siminski <laurensiminski@gmail.com>
2020-06-13 15:02:16 -0400

    initial files setup

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// 1. navigate through the single HTML file
+
+	// hide quiz questions and summary and only show intro
+
+	// on start button click, hide intro and show first question
+
+	// after all questions have been answered, hide questions and show summary
+	
+// 2. timer function
+
+	// on start button click, start 60 second timer
+	
+	// capture timer number when the correct last question button is clicked
+	
+// 3. circle through questions
+
+	// display first quesiton from the list of questions
+	
+	// if wrong answer is selected, alert "Wrong!"
+	
+	// if wrong answer is selected, deduct 10 seconds from timer
+
+	// if correct answer is selected, alert "Correct!"
+	
+	// if correct answer is selected, display next question
+	
+// 4. local storage
+
+	// store name and score when submit score button is clicked
